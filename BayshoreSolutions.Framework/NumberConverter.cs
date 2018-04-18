db89cf07113358dc1adcc54b7f88c9e3a668f89c
Blake Shadle <MASTERBRAIN\blake>
2018-04-18 16:45:34 +0000

    adding bayshore code test to solution
    git-tfs-id: [http://localhost:8080/tfs/DefaultCollection]$/DevProject/BayshoreSolutions;C653

---
+++ BayshoreSolutions.Framework/NumberConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BayshoreSolutions.Framework
+{
+    public class NumberConverter
+    {
+        private float number;
+
+        public NumberConverter(float _number)
+        {
+            number = _number;
+        }
+
+        public string Convert()
+        {
+            return "";
+        }
+
+
+    }
+}
